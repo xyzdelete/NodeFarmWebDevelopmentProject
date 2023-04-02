64fd7356605a2194c2db347d3dcbff05410c64bc
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-02 16:00:30 +0300

    Parsing Variables from URLs

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
-    if (pathName === "/" || pathName === "/overview") {
+    if (pathname === "/" || pathname === "/overview") {
         res.writeHead(200, {
             "Content-type": "text/html",
         });
@@ -86,11 +86,16 @@ const server = http.createServer((req, res) => {
         res.end(output);
     }
     // Product page
-    else if (pathName === "/product") {
-        res.end("This is the PRODUCT");
+    else if (pathname === "/product") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
     // API page
-    else if (pathName === "/api") {
+    else if (pathname === "/api") {
         res.writeHead(200, {
             "Content-type": "application/json",
         });
