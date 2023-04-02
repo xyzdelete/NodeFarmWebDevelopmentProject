aa3520f1aa94571f2a963641ae4723b2eff1425a
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-02 14:11:52 +0300

    Building a (Very) Simple API

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ const url = require("url");
 
 /////////////////////////////////////////
 // Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -44,6 +47,11 @@ const server = http.createServer((req, res) => {
         res.end("This is the OVERVIEW");
     } else if (pathName === "/product") {
         res.end("This is the PRODUCT");
+    } else if (pathName === "/api") {
+        res.writeHead(200, {
+            "Content-type": "application/json",
+        });
+        res.end(data);
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
