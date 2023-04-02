3632829aa01f1235a838989b01c6bc61061d58f8
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-02 13:50:14 +0300

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 /////////////////////////////////////////
 // Files
@@ -37,8 +38,19 @@ const http = require("http");
 /////////////////////////////////////////
 // Server
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end("Hello from the server!");
+    const pathName = req.url;
+
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("This is the PRODUCT");
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world",
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
 });
 
 server.listen(8000, "127.0.0.1", () => {
