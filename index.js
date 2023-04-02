1e084b2901ea79437dcd58492d39478e9eedaadd
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-02 13:33:29 +0300

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+/////////////////////////////////////////
+// Files
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -9,23 +13,34 @@ const fs = require("fs");
 // console.log("File written!");
 
 // Non-Blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    if (err) return console.log("ERROR! 💥");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     if (err) return console.log("ERROR! 💥");
 
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-            console.log(data3);
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile(
-                "./txt/final.txt",
-                `${data2}\n${data3}`,
-                "utf-8",
-                (err) => {
-                    console.log("Your file has been written ✅");
-                }
-            );
-        });
-    });
+//             fs.writeFile(
+//                 "./txt/final.txt",
+//                 `${data2}\n${data3}`,
+//                 "utf-8",
+//                 (err) => {
+//                     console.log("Your file has been written ✅");
+//                 }
+//             );
+//         });
+//     });
+// });
+// console.log("Will read file!");
+
+/////////////////////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 8000");
+});
